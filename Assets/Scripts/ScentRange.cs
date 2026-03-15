f4e501f4a22d673813419c8bd5c22a62504ec534
Peter Morrison <petermorrison250@gmail.com>
2026-03-15 16:03:08 +0000

    Add ScentRange model to hold the two returned points when tracking

---
+++ Assets/Scripts/ScentRange.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace LastKnownPosition
+{
+    public class ScentRange
+    {
+        public Vector2 PointA { get; }
+        public Vector2 PointB { get; }
+
+        public ScentRange(Vector2 pointA, Vector2 pointB)
+        {
+            PointA = pointA;
+            PointB = pointB;
+        }
+    }
+}
