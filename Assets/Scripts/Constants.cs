7bfca2d4e447d53fa6f251e5c196f636398123d7
Peter Morrison <petermorrison250@gmail.com>
2026-03-15 17:11:33 +0000

    Make ring offset very large to prevent large rings colliders from interacting with player

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,6 +3,6 @@
     public class Constants
     {
         public static readonly float BaseDirectionRange = 36;
-        public static readonly float RingOffset = -15f;
+        public static readonly float RingOffset = -1000f;
     }
 }
