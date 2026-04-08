5d80f59f6b8115118dd0905f219c48e189ec58f4
AntoniaF484 <antoniaforsythe16@gmail.com>
2026-04-08 14:22:12 +0100

    Player Controls updated
    Updated to charactercontroller to prevent player moving through steep slopes

--- Assets/Scripts/FirstPersonPlayer.cs
+++ Assets/Scripts/FirstPersonPlayer.cs
@@ -16,10 +16,13 @@ public class FirstPersonPlayer : MonoBehaviour
 //For character movement
     public float speed;
     Vector3 movementDirection;
-
-    private Rigidbody rb;
-    public float jumpForce;
+    private CharacterController characterController;
+    Vector3 velocity;
     bool isGrounded;
+    public Transform groundCheck;
+    public float gravity;
+    public LayerMask groundMask;
+    public float groundDistance;
 
     public event EventHandler MissingPersonFound;
     
@@ -28,15 +31,15 @@ public class FirstPersonPlayer : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked; // locks cursor to center of game view during play (hit esc to exit)
-        rb=GetComponent<Rigidbody>(); 
+       // rb=GetComponent<Rigidbody>(); 
         isGrounded=true;
+        characterController = GetComponent<CharacterController>();
 
     }
 
     void Update()
     {
         CameraView();
-        Jump();
         PlayerMovement();
 
     }
@@ -60,35 +63,38 @@ public class FirstPersonPlayer : MonoBehaviour
 
     public void PlayerMovement()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+       isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+       if (isGrounded && velocity.y < 0)
+       {
+           velocity.y = -2f;
+       }
+       
+       float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        
+        
+        Vector3 movementDirection = transform.right * horizontal + transform.forward * vertical;
+        characterController.Move(movementDirection * speed * Time.deltaTime);
 
-        movementDirection = transform.right * horizontal + transform.forward * vertical;
+       velocity.y += gravity * Time.deltaTime;
+       characterController.Move(velocity * Time.deltaTime);
 
-        rb.MovePosition(rb.position + movementDirection * (speed * Time.deltaTime));
+      
         
     }
 
-    private void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }
-    }
+   
 
     
-    private void OnCollisionEnter(Collision collision)
+    private void OnControllerColliderHit (ControllerColliderHit hit)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
+       
 
-        if (collision.gameObject.CompareTag("MissingPerson"))
+        if (hit.gameObject.CompareTag("MissingPerson"))
         {
             MissingPersonFound?.Invoke(this, EventArgs.Empty);
+            Debug.unityLogger.Log("missing person found");
         }
     }
 }
