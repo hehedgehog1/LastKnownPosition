c4a83004139fe4878b6768fe009e18516a9f31e3
hehedgehog1 <154544566+hehedgehog1@users.noreply.github.com>
2026-04-09 17:11:53 +0100

    map marker follows player

---
+++ Assets/Scripts/MapMarker.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MapMarker : MonoBehaviour
+{
+    public RectTransform playerMarker;
+    public Transform player;
+    public RectTransform mapRect;
+    public Vector2 mapWorldSize;
+    public float offsetX = 5f;
+    public float offsetY = -3f;
+
+
+    void Update()
+    {
+        if (!mapRect.gameObject.activeSelf) return;
+
+        Vector3 playerPos = player.position;
+
+        float x = (1 - (player.position.z / mapWorldSize.y)) * mapRect.rect.width;
+        float y = (player.position.x / mapWorldSize.x) * mapRect.rect.height;
+
+        Vector2 offset = new Vector2(offsetX, offsetY); // tweak for map offset
+
+        playerMarker.anchoredPosition = new Vector2(x, y) + offset;
+    }
+}
