fec7d09695486e7031112ba0a56e72192427a481
hehedgehog1 <154544566+hehedgehog1@users.noreply.github.com>
2026-04-09 15:32:46 +0100

    added map to UI

--- Assets/Scripts/MapCapture.cs
+++ Assets/Scripts/MapCapture.cs
@@ -18,12 +18,19 @@ public class MapCapture : MonoBehaviour
 
         mapCamera.Render();
 
-        Texture2D image = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+        Texture2D image = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false, true);
         image.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+
+        // Convert linear colors to sRGB to fix dark PNG
+        Color[] pixels = image.GetPixels();
+        for (int i = 0; i < pixels.Length; i++)
+            pixels[i] = pixels[i].gamma;
+        image.SetPixels(pixels);
+
         image.Apply();
 
-        byte[] bytes = image.EncodeToPNG();
-        File.WriteAllBytes(Application.dataPath + "/MapImage.png", bytes);
+        string filePath = Application.dataPath + "/Art/Map/MapImage.png";
+        File.WriteAllBytes(filePath, image.EncodeToPNG());
 
         RenderTexture.active = currentRT;
 
