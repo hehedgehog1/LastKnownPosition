106b0380067023f172b2c483891423c8d737b194
Peter Morrison <petermorrison250@gmail.com>
2026-04-18 10:29:32 +0100

    Add Dog Whistle when commanding

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,9 +4,32 @@ public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip radio;
+    [SerializeField] private AudioClip whistle;
+    
+    public static SoundManager Instance;
 
+    private void Awake()
+    {
+        if (Instance is null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+    
     public void PlayRadio()
     {
         audioSource.PlayOneShot(radio);
+    }
+
+    public void PlayWhistle()
+    {
+        audioSource.PlayOneShot(whistle);
     }
 }
